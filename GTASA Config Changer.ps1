1d7866e9526c8f0e8688038103ee01e62aa6c020
Anakeen <anakeen_sniper_machine@hotmail.com>
2023-03-14 18:35:48 -0300

    First full working version of the script.

--- GTASA Config Changer.ps1
+++ GTASA Config Changer.ps1
@@ -1,31 +1,142 @@
-﻿# GTA SA Config Manager #
+﻿<# GTA SA Config Manager #>
 
 <# Variables #>
-$path10 = "F:\Speedrun\Grand Theft Auto San Andreas 1.01\back up\Game Config\1.0\gta_sa.set"
-$path101 = "F:\Speedrun\Grand Theft Auto San Andreas 1.01\back up\Game Config\1.01\gta_sa.set"
+$config10path = Get-Content .\Paths\path10.txt
+$config101path = Get-Content .\Paths\path101.txt
+$currentconfigfilepath = Get-Content .\Paths\configfilepath.txt
+$gtasauserfilespath = Get-Content .\Paths\gtasauserfilespath.txt
+$versionselected = ""
+$isconfigfileexists = ""
+$dxwndpath = Get-Content .\Paths\pathdxwnd.txt
+$gta10path = Get-Content .\Paths\pathgta10.txt
+$gta101path = Get-Content .\Paths\pathgta101.txt
 
 <# Functions #>
 Function ShowMenu {
-    Cls
+   Clear-Host
 
-    Write-Host "       GTA SA Config Changer       "
+    Write-Host -ForegroundColor DarkBlue -BackgroundColor Gray "       GTA SA Config File Manager       "
 
     Write-Host "`n"
 
-    Write-Host "------------------------------------"
+    Write-Host -ForegroundColor Yellow -BackgroundColor Black "--------------------------------------"
 
-    Write-Host "1. Install GTA SA 1.0 Configuration"
+    Write-Host "1. Change config file for GTA SA 1.0"
     
-    Write-Host "2. Install GTA SA 1.01 Configuration"
+    Write-Host "2. Change config file for GTA SA 1.01"
     
     Write-Host "3. Exit"
 
-    Write-Host "------------------------------------"
+    Write-Host -ForegroundColor Yellow -BackgroundColor Black "--------------------------------------"
 }
 
-Function InstallConfig {
-    
+Function InstallConfigFile {
+   Clear-Host
 
+   Write-Host "Checking if the GTA SA User Files folder exists...."
+   
+   Write-Host "`n"
+   
+   if ((Test-Path -Path $gtasauserfilespath) -ne "True") {
+      Clear-Host
+      
+      Write-Host -ForegroundColor Red "GTA SA User Files folder doesn't exists. Please make sure the folder exits before installing the config file"
+
+      Write-Host "`n"
+
+      Read-Host "Press any key to continue..."
+      
+      exit
+   }
+   else {
+
+      Write-Host -ForegroundColor Green "GTA SA User Files folder found"
+
+      Write-Host "`n"
+
+      Read-Host "Press any key to continue..."
+
+      $isconfigfileexists = ([System.IO.File]::Exists($currentconfigfilepath))
+
+      if ($isconfigfileexists -eq $true -or $isconfigfileexists -eq $false) {
+         if ($versionselected -eq "1.0") {
+            Write-Host "Proceeding to install" $versionselected "confing file into the GTA User Files folder....."
+
+            Copy-Item -Path $config10path -Destination $currentconfigfilepath
+
+            Write-Host "`n"
+
+            Write-Host -ForegroundColor Green $versionselected "confing file installed successfully"
+
+            $isconfigfileexists = ""
+
+            Write-Host "`n"
+
+            Read-Host "Press any key to continue..."
+            
+         }
+         else {
+            Write-Host "Proceeding to install" $versionselected "confing file into the GTA User Files folder....."
+
+            Copy-Item -Path $config101path -Destination $currentconfigfilepath
+
+            Write-Host "`n"
+
+            Write-Host -ForegroundColor Green $versionselected "confing file installed successfully"
+
+            $isconfigfileexists = ""
+
+            Write-Host "`n"
+
+            Read-Host "Press any key to continue..."
+         }
+      }
+   }
+}
+
+Function OpenDxwndGTASA {
+   Clear-Host
+
+   Write-Host "Opening Dxwnd...."
+
+   Start-Process -FilePath "dxwnd.exe" -WorkingDirectory $dxwndpath
+
+   Write-Host "`n"
+
+   Write-Host "Dxwnd opened successfully"
+
+   Write-Host "`n"
+
+   Read-Host "Press any key to continue..."
+
+   Write-Host "Opening Grand Theft Auto San Andreas "$versionselected "...."
+
+   if ($versionselected -eq "1.0") {
+      Write-Host "`n"
+
+      Start-Process -FilePath "gta_sa.exe" -WorkingDirectory $gta10path
+
+      Write-Host "Grand Theft Auto San Andreas" $versionselected "opened successfully"
+
+      $versionselected = ""
+
+      Write-Host "`n"
+
+      Read-Host "Press any key to continue..."
+   }
+   else {
+      Write-Host "`n"
+
+      Start-Process -FilePath "gta_sa.exe" -WorkingDirectory $gta101path
+
+      Write-Host "Grand Theft Auto San Andreas" $versionselected "opened successfully"
+
+      $versionselected = ""
+
+      Write-Host "`n"
+
+      Read-Host "Press any key to continue..."
+   }
 }
 
 <# Main #>
@@ -38,20 +149,83 @@ do {
 
     switch ($option) {
           '1' {
-             Cls
+             Clear-Host
 
-             Write-Host "You chosen to Install GTA SA 1.0 Configuration"
+             Write-Host -ForegroundColor Green "You chose to Install GTA SA 1.0 Configuration"
+
+             $versionselected = "1.0"
+
+             Write-Host "`n"
+
+             Write-Host "The location of your backup file for GTA SA 1.0 is: " $config10path
+
+             Write-Host "`n"
+
+             do { 
+               $confirmation = Read-Host "Are you Sure that is the correct backup path? [y/n]" 
+
+               if ($confirmation -eq 'n') {
+                  Clear-Host
+                  
+                  Write-Host -ForegroundColor Yellow "Please review the path located in the text file for GTA SA 1.0 before installing the config file"
+
+                  Write-Host "`n"
+
+                  Read-Host "Press any key to continue..."
+
+                  exit
+               } 
+            }
+            while($confirmation -ne "y")
+
+            InstallConfigFile
+
+            OpenDxwndGTASA
 
           }
           '2' {
-             Cls
+            Clear-Host
 
-             Write-Host "You chosen to Install GTA SA 1.01 Configuration"
+             Write-Host -ForegroundColor Green "You chose to Install GTA SA 1.01 Configuration"
+
+             $versionselected = "1.01"
+
+             Write-Host "`n"
+
+             Write-Host "The location of your backup file for SA 1.01 is: " $config101path
+
+             Write-Host "`n"
+
+             do { 
+               $confirmation = Read-Host "Are you Sure that is the correct backup path? [y/n]" 
+
+               if ($confirmation -eq 'n') {
+                  Clear-Host
+                  
+                  Write-Host -ForegroundColor Yellow "Please review the path located in the text file for GTA SA 1.01 before installing the config file"
+
+                  Write-Host "`n"
+
+                  Read-Host "Press any key to continue..."
+
+                  exit
+               } 
+            }
+            while($confirmation -ne "y")
+
+            InstallConfigFile
+            
+            OpenDxwndGTASA
+            
           }
           '3' {
-             Cls
+            Clear-Host
 
-             Write-host "Exiting the program!"
+             Write-host "You exit the program"
+
+             Write-Host "`n"
+
+             Write-host "See you next time!"
 
              Write-Host "`n"
 
@@ -60,15 +234,16 @@ do {
              exit
           }
           Default {
-                 Cls
+             Clear-Host
                  
-                 Write-Host -ForegroundColor red -BackgroundColor white "Invalid option. Please choose another option"
-          
-          }
+             Write-Host -ForegroundColor Red "Invalid option. Please choose another option"
+
+             Write-Host "`n"
+
+             Read-Host "Press any key to continue..."
+
+             ShowMenu
+        }
     }
 } 
-while ($option -eq '3')
-
-Write-Host "`n"
-
-Read-Host "Press any key to continue..."
+until ($option -eq '3')
